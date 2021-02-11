1626b5a0f423b6575ec69f23d7b9714414d43669
Kristopher Overholt <kris@koverholt.com>
2021-02-10 23:21:00 -0600

    Debug

--- site/js/weather-dashboard.js
+++ site/js/weather-dashboard.js
@@ -36,13 +36,14 @@ var param = new Vue({
 var input_location = window.value || "Austin, TX";
 var input = JSON.stringify({"location": input_location});
 
+console.log(input);
+
 var request = new XMLHttpRequest()
 request.open('POST', 'https://us-central1-koverholt-apps-304316.cloudfunctions.net/weather-dashboard', true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(input);
 
 request.onload = function () {
-    console.log(this.response);
     var obj = JSON.parse(this.response)
     var location_not_found = obj["location_not_found"];
     var village = obj["village"];
